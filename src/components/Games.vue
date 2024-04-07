8f55f847009e7c6c336e7c40acff84c134dcb8d8
Jordan Welch <jordanhwelch@gmail.com>
2024-04-07 16:06:08 -0500

    Make Games sortable

--- src/components/Games.vue
+++ src/components/Games.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-import { reactive, watch } from 'vue'
+import { reactive, ref, watch } from 'vue'
 import { defaultConfig } from '../config/defaultConfig'
 import { Config, isConfig } from '../config/config'
 import { id } from '../utils/strings'
-import { XMarkIcon } from '@heroicons/vue/24/solid'
+import { Bars3Icon, XMarkIcon } from '@heroicons/vue/24/solid'
+import { useSortable } from '@vueuse/integrations/useSortable'
 
 const localConfig = JSON.parse(localStorage.getItem('config') ?? '{}')
 const config = reactive<Config>(isConfig(localConfig) ? localConfig : defaultConfig)
@@ -15,10 +16,18 @@ watch(config, (newConfig) => {
 const removeGame = (title: string) => {
   config.games = config.games.filter(game => game.title !== title)
 }
+
+const gamesList = ref<HTMLElement | null>(null)
+useSortable(gamesList, config.games, {
+  handle: '.handle',
+})
 </script>
 
 <template>
-  <ul class="flex flex-col flex-grow max-w-sm p-4 space-y-4 text-white">
+  <ul
+    ref="gamesList"
+    class="flex flex-col flex-grow max-w-sm p-4 space-y-4 text-white"
+  >
     <li
       v-for="game in config.games"
       :key="game.title"
@@ -28,9 +37,13 @@ const removeGame = (title: string) => {
         target="blank"
         class="flex justify-between w-full h-full p-4 align-middle border cursor-pointer hover:underline"
       >
-        <span>
-          {{ game.title }}
-        </span>
+        <div class="flex items-center space-x-2">
+          <Bars3Icon class="w-4 h-4 handle" />
+
+          <span>
+            {{ game.title }}
+          </span>
+        </div>
 
         <button
           :id="'remove-' + id(game.title)"
