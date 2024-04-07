217783e973c534ded1652c230a64c68d99fabebb
Jordan Welch <jordanhwelch@gmail.com>
2024-04-07 11:29:37 -0500

    Link to games

--- src/components/Games.vue
+++ src/components/Games.vue
@@ -2,24 +2,31 @@
 const games = [
   {
     title: 'Wordle',
+    url: 'https://www.nytimes.com/games/wordle/index.html',
   },
   {
     title: 'Connections',
+    url: 'https://www.nytimes.com/games/connections',
   },
   {
     title: 'Mini Crossword',
+    url: 'https://www.nytimes.com/crosswords/game/mini',
   },
   {
     title: 'Crossword',
+    url: 'https://www.nytimes.com/crosswords',
   },
   {
     title: 'Cinematrix',
+    url: 'https://www.vulture.com/article/daily-movie-grid-trivia-game-cinematrix.html',
   },
   {
     title: 'Framed',
+    url: 'https://framed.wtf/',
   },
   {
     title: 'Spotle',
+    url: 'https://spotle.io/',
   },
 ]
 </script>
@@ -30,7 +37,12 @@ const games = [
       v-for="game in games"
       :key="game.title"
     >
-      {{ game.title }}
+      <a
+        :href="game.url"
+        target="blank"
+      >
+        {{ game.title }}
+      </a>
     </li>
   </ul>
 </template>
