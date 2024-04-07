d1a7d43c05acef430ef507e5c269f353ee0d4265
Jordan Welch <jordanhwelch@gmail.com>
2024-04-07 13:56:19 -0500

    extract config

--- src/components/Games.vue
+++ src/components/Games.vue
@@ -1,34 +1,5 @@
 <script setup lang="ts">
-const games = [
-  {
-    title: 'Wordle',
-    url: 'https://www.nytimes.com/games/wordle/index.html',
-  },
-  {
-    title: 'Connections',
-    url: 'https://www.nytimes.com/games/connections',
-  },
-  {
-    title: 'Mini Crossword',
-    url: 'https://www.nytimes.com/crosswords/game/mini',
-  },
-  {
-    title: 'Crossword',
-    url: 'https://www.nytimes.com/crosswords',
-  },
-  {
-    title: 'Cinematrix',
-    url: 'https://www.vulture.com/article/daily-movie-grid-trivia-game-cinematrix.html',
-  },
-  {
-    title: 'Framed',
-    url: 'https://framed.wtf/',
-  },
-  {
-    title: 'Spotle',
-    url: 'https://spotle.io/',
-  },
-]
+import { games } from '../config/defaultConfig'
 </script>
 
 <template>
---
+++ src/config/defaultConfig.ts
@@ -0,0 +1,30 @@
+export const games = [
+  {
+    title: 'Wordle',
+    url: 'https://www.nytimes.com/games/wordle/index.html',
+  },
+  {
+    title: 'Connections',
+    url: 'https://www.nytimes.com/games/connections',
+  },
+  {
+    title: 'Mini Crossword',
+    url: 'https://www.nytimes.com/crosswords/game/mini',
+  },
+  {
+    title: 'Crossword',
+    url: 'https://www.nytimes.com/crosswords',
+  },
+  {
+    title: 'Cinematrix',
+    url: 'https://www.vulture.com/article/daily-movie-grid-trivia-game-cinematrix.html',
+  },
+  {
+    title: 'Framed',
+    url: 'https://framed.wtf/',
+  },
+  {
+    title: 'Spotle',
+    url: 'https://spotle.io/',
+  },
+]
