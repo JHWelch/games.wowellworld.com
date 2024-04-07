ff3d2e1c94fd41bff89f0f29328664cbc8448e26
Jordan Welch <jordanhwelch@gmail.com>
2024-04-07 13:40:13 -0500

    Use vite-plugin-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vitest/config'
+import { VitePWA } from 'vite-plugin-pwa'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    VitePWA({ registerType: 'autoUpdate' }),
+    vue(),
+  ],
   test: {
     environment: 'jsdom',
   },
