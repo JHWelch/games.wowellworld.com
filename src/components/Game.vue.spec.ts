34132071afcac6e6ac4f561ea4d9eac65c3b6ff3
Jordan Welch <jordanhwelch@gmail.com>
2024-04-07 20:39:57 -0500

    Add game tests

---
+++ src/components/Game.vue.spec.ts
@@ -0,0 +1,65 @@
+import { VueWrapper, mount } from '@vue/test-utils'
+import Game from './Game.vue'
+import { it, expect, describe, beforeAll, vi } from 'vitest'
+
+let wrapper: VueWrapper
+
+const game = {
+  title: 'Wordle',
+  url: 'https://www.nytimes.com/games/wordle/index.html',
+}
+
+const removeGame = vi.fn()
+
+const props = {
+  edit: false,
+  game,
+  removeGame,
+}
+
+it('shows all game title', () => {
+  const text = mount(Game, { props }).text()
+
+  expect(text).toContain('Wordle')
+})
+
+describe('edit enabled', () => {
+  beforeAll(async () => {
+    wrapper = mount(Game, { props: {
+      ...props,
+      edit: true,
+    } })
+  })
+
+  it('displays remove icon', () => {
+    expect(wrapper.find('#remove-wordle').exists()).toBe(true)
+  })
+
+  it('displays sort handle', () => {
+    expect(wrapper.find('.handle').exists()).toBe(true)
+  })
+
+  describe('click remove icon on game', () => {
+    beforeAll(async () => {
+      await wrapper.find('#remove-wordle').trigger('click')
+    })
+
+    it('calls removeGame', () => {
+      expect(removeGame).toHaveBeenCalled()
+    })
+  })
+})
+
+describe('edit disabled', () => {
+  beforeAll(() => {
+    wrapper = mount(Game, { props })
+  })
+
+  it('does not display remove icon', () => {
+    expect(wrapper.find('#remove-wordle').exists()).toBe(false)
+  })
+
+  it('does not display sort handles', () => {
+    expect(wrapper.findAll('.handle').length).toBe(0)
+  })
+})
