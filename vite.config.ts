0f7944cef1e9068ac399d82da1bdc50dee5cdd5a
Jordan Welch <jordanhwelch@gmail.com>
2024-04-11 23:01:35 -0500

    Run and add generated assets

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,39 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [
     VitePWA({
+      includeAssets: [
+        'favicon.ico',
+        'robots.txt',
+        'apple-touch-icon-180x180.png',
+        'maskable-icon-512x512.png',
+        'logo.svg',
+      ],
       registerType: 'autoUpdate',
       manifest: {
         theme_color: '#171717',
+        icons: [
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
       },
     }),
     vue(),
