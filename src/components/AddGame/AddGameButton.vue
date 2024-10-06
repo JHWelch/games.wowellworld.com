3f704b225c62f0a41bd71fdf0a01723e6081f878
Jordan Welch <jordanhwelch@gmail.com>
2024-10-05 20:09:48 -0500

    tearoff is angry

--- src/components/AddGame/AddGameButton.vue
+++ src/components/AddGame/AddGameButton.vue
@@ -7,7 +7,7 @@ import { addGameModal } from '../../state/addGameModalState'
   <button
     id="add-game"
     class="flex items-center justify-center w-full h-full p-4 space-x-2 text-xl align-middle border-2 border-dashed rounded-lg cursor-pointer backdrop-contrast-75"
-    @click="addGameModal.open"
+    @click="() => addGameModal.open()"
   >
     <PlusIcon class="w-6 h-6 stroke-[3px]" />
 
